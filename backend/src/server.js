bac9acad75360de7b0aabf78fd6ad7a224bdbaa9
tai <trantai09032003@gmail.com>
2026-04-05 10:56:14 +0700

    Configure app for production startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoute.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
@@ -9,11 +11,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
 
 // middleware
-app.use(cors({
-  origin: "http://localhost:5173", // allow requests from this origin
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // allow requests from this origin
+    }),
+  );
+}
+
 app.use(express.json()); // this middleware will parse json bodies: req.body
 app.use(rateLimiter); // apply rate limiter to all routes
 // app.use((req, res, next) => {
@@ -22,6 +32,14 @@ app.use(rateLimiter); // apply rate limiter to all routes
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(frontendDistPath));
+
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// in production, the frontend will be served from the same origin as the backend, so we can use a relative URL
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
